e555173c30c4bcc0032a7f518173b80ca8913b49
imanthij <imanthij@scienter.lk>
2025-02-26 12:56:24 +0530

    Done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,86 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'Ang19CURDWithLocalStorage';
-  isNewUser: boolean = false;
+  isNewUser: boolean = true;
+  userObj: User = new User();
+  localStorageKey: string = 'userDetails';
+  userList: User[] = [];
+
+  ngOnInit() {
+    const localData = localStorage.getItem(this.localStorageKey);
+    this.userList = localData ? JSON.parse(localData) : [];
+  }
+
+  changeCard() {
+    this.isNewUser = !this.isNewUser;
+  }
+
+  onSave() {
+    debugger;
+    this.userObj.userId = this.userList.length + 1;
+    this.userList.push(this.userObj);
+    this.userObj = new User();
+    localStorage.setItem(this.localStorageKey, JSON.stringify(this.userList));
+    this.changeCard();
+  }
+
+  onUpdate() {
+    const record = this.userList.find((m) => m.userId == this.userObj.userId);
+    if (record != undefined) {
+      record.firstName = this.userObj.firstName;
+      record.lastName = this.userObj.lastName;
+      record.address = this.userObj.address;
+      record.gender = this.userObj.gender;
+      record.phoneNumber = this.userObj.phoneNumber;
+      record.zipCode = this.userObj.zipCode;
+    }
+
+    localStorage.setItem(this.localStorageKey, JSON.stringify(this.userList));
+    this.changeCard();
+  }
+
+  onEdit(user: any) {
+    this.userObj = user;
+    this.changeCard();
+  }
+
+  onDelete(userId: number) {
+    const isDelete = confirm('Are you Sure Delete??');
+    if (isDelete) {
+      const index = this.userList.findIndex((m) => m.userId == userId);
+      this.userList.splice(index, 1);
+      localStorage.setItem(this.localStorageKey, JSON.stringify(this.userList));
+    }
+  }
+}
+
+class User {
+  userId: number;
+  firstName: string;
+  lastName: string;
+  address: string;
+  phoneNumber: string;
+  gender: string;
+  zipCode: string;
+  isAgreed: boolean;
+
+  constructor() {
+    this.userId = 0;
+    this.firstName = '';
+    this.lastName = '';
+    this.address = '';
+    this.phoneNumber = '';
+    this.gender = '';
+    this.zipCode = '';
+    this.isAgreed = false;
+  }
 }
